838280da66b9cf07b8d1a61b2f8a0e9eab1da0c7
Madhavaprasath <Madhavprasath088@gmail.com>
2021-05-25 00:55:10 +0530

    adding update date and adding day night cycles

---
+++ game/Game elements/Game_clock.gd
@@ -0,0 +1,38 @@
+extends Node2D
+class_name Calender
+
+
+
+const time_zones=["Day","Night"]
+
+onready var current_data=OS.get_date()
+
+func _ready():
+	pass
+
+
+func update_date():
+	if current_data["month"] in [1,3,5,7,8,10,12]:
+		change_time(31)
+	elif current_data["month"] in [2]:
+		change_time(28)
+	else:
+		change_time(30)
+	print(current_data)
+
+func change_time(maximum_date):
+	if current_data["month"]<=12:
+		if current_data["day"] <maximum_date:
+				current_data["day"]+=1
+		else:
+			current_data["month"]+=1
+			current_data["day"]=1
+	else:
+		current_data["month"]=1
+		current_data["year"]+=1
+		
+
+
+func get_current_date():
+	return current_data
+
